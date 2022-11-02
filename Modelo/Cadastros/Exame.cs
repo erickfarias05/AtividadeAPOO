4175b55b2687a7a7f6e99ce4de4639f90ebb0102
Erick Santos Umbelino de Farias <erick.f@academico.ifrn.edu.br>
2022-11-02 13:15:45 -0300

    Adicionar arquivos de projeto.

---
+++ Modelo/Cadastros/Exame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Modelo.Cadastros
+{
+    public class Exame
+    {
+        public long ExameId { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
---
+++ Persistencia/Contexts/EFContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Modelo.Cadastros;
+
+namespace Persistencia.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+                new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+
+        public DbSet<Exame> Exames { get; set; }
+
+
+    }
+}
